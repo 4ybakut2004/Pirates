26913b68826ca30ae12d0b3da375bf5a2c12f8a2
Ringo-Freek <hitting@yandex.ru>
2013-08-03 13:28:57 +0700

    water star

--- gl_goal.js
+++ gl_goal.js
@@ -6,7 +6,7 @@ var posx_m; 									//x монстрика
 var posy_m;										//y монстрика
 var bal;										//количество, собранных монеток!
 
-var k = 37;										//количество буферов (по количеству кратинок)
+var k = 38;										//количество буферов (по количеству кратинок)
 var flag;										//флаг прыжка
 var fl_p;										//флаг pause
 var rab;										//номер исходной картинки
@@ -47,10 +47,14 @@ var code;										//не перезаписыват!!
 
 var met;										//метры
 var his;										//истории
+var cl_st;										//закрыта ли история		
 
 var mas_shu = new Array();						//координаты по у шупальцев
+var s_x, s_y;									//координаты звезды
+var fl_shit;									//защита от бомб
+var go;											//пробег с аурой
 
-var mas_point = new Array(20, 50, 100, 300, 600); 
+var mas_point = new Array(20, 50, 100, 300, 700); 
 var mas_pr = new Array(							//массив правил
 "Сперва выберите персонажа, за которого будете играть. Для начала нашего славного приключения нажмите клавишу d. Бежать от врага тебе поможет та же самая клавиша. А клавиши w и s помогут тебе уворачиваться от бомб и ловить монетки столько ценные в нашем деле. На пути тебе буду встречать якори и бревна, ",
 "увиливай от них, иначе предеться начать заново! Следи за черепками, что появляются в правом углу экрана, чем больше их, тем ближе ты к открытию новой части истории. Для каждого героя своя легенда! Собери все!<br/>  Открытые истории закрывают при нажатии на enter!",
@@ -79,8 +83,9 @@ function handleKeys()
 	if (currentlyPressedKeys[80] && flag) 
 	{
 		Pause();
+		currentlyPressedKeys[80] = false;
 	}
-	if(document.getElementById('over').style.display!='block'&&flag&&fl_p)
+	if(document.getElementById('over').style.display!='block'&&flag&&fl_p&&!cl_st)
 	{
 		if (currentlyPressedKeys[68]) 
 		{
@@ -90,6 +95,8 @@ function handleKeys()
 			move_obj(kol_b, s_b, mas_br);
 			move_obj(kol_br, s_br, mas_brev);
 			move_obj(kol_ya, s_ya, mas_yac);
+			if(fl_shit) go += s;
+			s_x--;
 			met += s;
 			for(var f = 0; f<8; f++)
 			{
@@ -169,7 +176,7 @@ function init_GL()
 	
 	//---------------------бревна инит----------------------------------------------------
 	kol_br = Math.floor((Math.random()*2)+1);
-	mas_brev[0] = Math.floor((Math.random()*700)+600);
+	mas_brev[0] = Math.floor((Math.random()*700)+700);
 	for(var i = 1; i < kol_br; i++)
 	{
 		mas_brev[i] = mas_brev[i-1] + Math.floor((Math.random()*300)+100);
@@ -251,7 +258,22 @@ function createTex(image_name)
 	}
 	return texture;
 }
-
+//---------------------------повторы--------------------------------------------------
+function pov(px, py, w, h, nu)
+{
+	gl.bindTexture(gl.TEXTURE_2D, texture[nu]);
+	gl.uniform1i(shaderProgram.samplerUniform, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[nu]);
+					
+	setRectangle(gl, px, py, w, h);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
+					
+	gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
+	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
+					
+	gl.drawArrays(gl.TRIANGLES,0,6);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+}
 //---------------------------отрисовка------------------------------------------------
 var pos = 0;
 var kon = 20;
@@ -260,6 +282,7 @@ function drawScene()
 {	
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	
+	if(go>1200){ fl_shit = 0; go = 0; }															//халява кончилась
 	for(var j = 0; j<5; j++)
 	{
 		if(bal==mas_point[j]&&!fl_bl)
@@ -276,19 +299,20 @@ function drawScene()
 		}
 		if(bal==mas_point[j]+1) fl_bl = 0;
 	}
-	
 	if(rab != 0)													//выбран ли герой!
 	{
 		if(posx_g-80<=posx_m&&posx_g>=posx_m||fl_over) gameOver();	//не пора ли заканчивать?)
 		else
 		{
-			if(flag&&fl_p)											//можно ли нам двигать?)
+			if(flag&&fl_p&&!cl_st)									//можно ли нам двигать?)
 			{	
 				move_obj(kol, s_m, mas_mon);
 				move_obj(11, s, mas_fon);
 				move_obj(kol_b, s_b, mas_br);
 				move_obj(kol_br, s_br, mas_brev);
 				move_obj(kol_ya, s_ya, mas_yac);
+				if(fl_shit) go += s;
+				s_y++;
 				met += s;
 				for(var f = 0; f<8; f++)
 				{
@@ -325,18 +349,7 @@ function drawScene()
 			{
 				if(mas_fon_fl[i])
 				{
-					gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-					gl.uniform1i(shaderProgram.samplerUniform, 0);
-					gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-					
-					setRectangle(gl, mas_fon[i], 0, 700, 600);
-					gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-					
-					gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-					gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-					
-					gl.drawArrays(gl.TRIANGLES,0,6);
-					gl.bindTexture(gl.TEXTURE_2D, null);
+					pov(mas_fon[i], 0, 700, 700, i);
 				}
 				if(mas_fon[i] < -700)
 				{		
@@ -369,19 +382,7 @@ function drawScene()
 						document.getElementById("bal").innerHTML = bal;
 						mas_fl[q] = 0;	
 					}
-					gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-					gl.uniform1i(shaderProgram.samplerUniform, 0);
-				
-					gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-					
-					setRectangle(gl, mas_mon[q], y_m, 50, 50);
-					gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-					
-					gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-					gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-					
-					gl.drawArrays(gl.TRIANGLES,0,6);
-					gl.bindTexture(gl.TEXTURE_2D, null);
+					pov(mas_mon[q], y_m, 50, 50, i);
 				}
 			}
 			//---------------------монстр----------------------------------------------------------
@@ -389,43 +390,28 @@ function drawScene()
 			var f = 0;
 			for(var gk = i; gk < k - 1; gk++)
 			{
-				gl.bindTexture(gl.TEXTURE_2D, texture[gk]);
-				gl.uniform1i(shaderProgram.samplerUniform, 0);
-
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[gk]);
-				if(gk==26) setRectangle(gl,posx_m, posy_m, 200, 200);
-				else { setRectangle(gl,posx_m, mas_shu[f], 200, 200); f++};
-				gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-					
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-				gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-					
-				gl.drawArrays(gl.TRIANGLES,0,6);
-				gl.bindTexture(gl.TEXTURE_2D, null);
+				if(gk==26) pov(posx_m, posy_m, 200, 200, gk);
+				else { pov(posx_m, mas_shu[f], 200, 200, gk); f++};
 			}
 			
 			//---------------------герой--------------------------------------------------------
-			i = rab;
-			gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-			gl.uniform1i(shaderProgram.samplerUniform, 0);
-
-			gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-			setRectangle(gl,posx_g,posy_g, 100, 100);
-			gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-				
-			gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-			gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-				
-			gl.drawArrays(gl.TRIANGLES,0,6);
-			gl.bindTexture(gl.TEXTURE_2D, null);
+			pov(posx_g,posy_g, 100, 100, rab);
 			
 			//---------------------бомбы---------------------------------------------------------
 			i = 24;
+			
 			if(mas_br[kol_b - 1] < -100)
 			{
+				var asd = 0;
+				for(var q = 0; q<kol_b; q++)
+				{
+					if(mas_br_fl[q]) asd++;
+				}
+				if(asd == kol_b) if(posx_m-10>=0) posx_m -=10;
 				kol_b = Math.floor((Math.random()*10)+1);
 				mas_br[0] = Math.floor((Math.random()*700)+500);
 				mas_br_y[0] = Math.floor((Math.random()*400)+10);
+				mas_br_fl[0] = 1;
 				for(var i = 1; i < kol_b; i++)
 				{
 					mas_br[i] = mas_br[i-1] + Math.floor((Math.random()*200)+50);
@@ -441,29 +427,17 @@ function drawScene()
 					if(posy_g>= mas_br_y[q]-80 && posy_g<= mas_br_y[q]+40 && posx_g<=mas_br[q] && posx_g>=-50+mas_br[q])
 					{
 						mas_br_fl[q] = 0;	
-						posx_m += 5;
+						if(!fl_shit) posx_m += 5; 													//если аура не поймана
 					}
-					gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-					gl.uniform1i(shaderProgram.samplerUniform, 0);
-				
-					gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-					
-					setRectangle(gl, mas_br[q], mas_br_y[q], 50, 50);
-					gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-					
-					gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-					gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-					
-					gl.drawArrays(gl.TRIANGLES,0,6);
-					gl.bindTexture(gl.TEXTURE_2D, null);
+					pov( mas_br[q], mas_br_y[q], 50, 50, i);
 				}
 			}
 			//-----------------------------------бревна---------------------------------------------------
-			i = k - 2;
+			i = k - 3;
 			if(mas_brev[kol_br - 1] < -200)
 			{
 				kol_br = Math.floor((Math.random()*2)+1);
-				mas_brev[0] = Math.floor((Math.random()*700)+600);
+				mas_brev[0] = Math.floor((Math.random()*700)+700);
 				for(var j = 1; j < kol_br; j++)
 				{
 					mas_brev[i] = mas_brev[i-1] + Math.floor((Math.random()*300)+100);
@@ -475,27 +449,15 @@ function drawScene()
 				{	
 					fl_over = 1;
 				}
-				gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-				gl.uniform1i(shaderProgram.samplerUniform, 0);
-						
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-							
-				setRectangle(gl, mas_brev[j], 370, 200, 100);
-				gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-							
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-				gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-							
-				gl.drawArrays(gl.TRIANGLES,0,6);
-				gl.bindTexture(gl.TEXTURE_2D, null);
+				pov(mas_brev[j], 370, 200, 100, i);
 			}
 			
 			//-----------------------------------якорь---------------------------------------------------
-			i = k - 1;
+			i = k - 2;
 			if(mas_yac[kol_ya - 1] < -100)
 			{
 				kol_ya = Math.floor((Math.random()*2)+1);
-				mas_yac[0] = Math.floor((Math.random()*700)+600); 
+				mas_yac[0] = Math.floor((Math.random()*700)+700); 
 				for(var j = 1; j < kol_ya; j++)
 				{
 					mas_yac[j] = mas_yac[j-1] + Math.floor((Math.random()*300)+100);
@@ -507,20 +469,28 @@ function drawScene()
 				{	
 					fl_over = 1;
 				}
-				gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-				gl.uniform1i(shaderProgram.samplerUniform, 0);
-						
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-							
-				setRectangle(gl, mas_yac[j], 0, 84, 200);
-				gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-							
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-				gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-							
-				gl.drawArrays(gl.TRIANGLES,0,6);
-				gl.bindTexture(gl.TEXTURE_2D, null);
+				pov(mas_yac[j], 0, 84, 200, i);
 			}
+			
+			//------------------------------бонус!!!------------------------------------------------------		
+			i = k - 1;
+			var ah = Math.floor((Math.random()*100) + 1);
+			if(s_y>400&&ah==2&&!fl_shit) 
+			{
+				s_x = Math.floor((Math.random()*700) + 300);
+				s_y = -70;
+			}
+			if(posx_g + 50 >= s_x && posx_g <= s_x && posy_g + 50 >= s_y &&posy_g <= s_y) 
+			{
+				s_y = 500;
+				fl_shit = 1;
+			}
+			pov(s_x, s_y, 50, 50, i);
+			if(fl_shit)
+			{
+				pov(600, 60, 25, 25, i);
+			}
+				
 			//------------------------------черепушечки----------------------------------------------------
 			i = 23;
 			if(pos==0 && bal==mas_point[pos]*kon/100)
@@ -531,19 +501,7 @@ function drawScene()
 			else if(bal-mas_point[pos-1]==(mas_point[pos]-mas_point[pos-1])*kon/100){ kl++; kon += 20;}
 			for(var l = 0; l < kl; l++)
 			{
-				gl.bindTexture(gl.TEXTURE_2D, texture[i]);
-				gl.uniform1i(shaderProgram.samplerUniform, 0);
-						
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexPositionBuffer[i]);
-							
-				setRectangle(gl, 580 - 50*l, 5, 50, 50);
-				gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,2,gl.FLOAT,false,0,0);
-							
-				gl.bindBuffer(gl.ARRAY_BUFFER, objVertexTextureCoordBuffer[0]);
-				gl.vertexAttribPointer(shaderProgram.textureCoordAttribute,2,gl.FLOAT,false,0,0);
-							
-				gl.drawArrays(gl.TRIANGLES,0,6);
-				gl.bindTexture(gl.TEXTURE_2D, null);
+				pov(580 - 50*l, 5, 50, 50, i);
 			}
 		}
 	}
@@ -602,6 +560,7 @@ function main()
 	
 	texture[35] = createTex("brev.png");
 	texture[36] = createTex("yac.png");
+	texture[37] = createTex("sv.png");
 	
 	document.getElementById('player').play();
 	document.onkeydown = handleKeyDown;
@@ -666,6 +625,7 @@ function Close()
 		fl_p = 1;
 		kon = 20;
 		kl = 0;
+		cl_st = 0;
 	}
 }
 function Close_about()
@@ -685,6 +645,7 @@ function story()
 	his++;
 	document.getElementById("p4").innerHTML = "Bоодушевляющая история!<br/> бла-бал-бла-бла-бла-бла-бла-бла-бла-бал-бла-бла-бла-бла";
 	document.getElementById('wind_story').style.display='block';
+	cl_st = 1;
 	return false;
 }
 //----------------------обнуление все, начинаем все заново!-----------------------------
@@ -712,6 +673,13 @@ function Start()
 	met = 0;
 	his = 0;
 	num_pr = 0;
+	cl_st = 0;
+	
+	fl_shit = 0;
+	go = 0;
+	
+	s_x = Math.floor((Math.random()*700) + 200);
+	s_y = -70;
 	
 	init_GL();
 	initShader();	
@@ -740,10 +708,11 @@ function gameOver()
 	document.getElementById("wind_bal").style.display = "none";
 	document.getElementById('over').style.display='block'; 
 	document.getElementById('code_vs').style.display='block'; 
-	met = (met - met%100)/100;
-	document.getElementById('p3').innerHTML = "Игра закончена!<br/><br/>Количество монет......" + bal + "<br/>Пройдено метров......." + met + "<br/>Открыто историй......." + his;
 	if(!code)
 	{
+		met = (met - met%100)/100;
+		document.getElementById('p3').innerHTML = "Игра закончена!<br/><br/>Количество монет......" + bal + "<br/>Пройдено метров......." + met + "<br/>Открыто историй......." + his;
+	
 		var s = "<textarea autofocus=\"autofocus\" id=\"inptext\" style=\"width:100%; height:100px; \" ><div style = \"width: 300px; height: 160px; color: #000; front-size: 10px; border-radius: 4px; background-color:#716a21; box-shadow: 2px 2px 6px #333, inset 0px 0px 10px #a89130; border: 1px solid #a18f52; filter: alpha(opacity=98); -moz-opacity: 0.98; -khtml-opacity: 0.98; opacity: 0.98;\" align=\"center\" >";
 		s += "<p style = \"margin-top: 15px;\">Ваш результат!</p><table cellpadding = 5> <tr><td>Количество монет</td><td width='100px' align=\"right\">" + bal + "</td></tr>";
 		s += "<td>Пройдено метров</td><td width='100px' align=\"right\"> " + met + "</td></tr><tr>";
